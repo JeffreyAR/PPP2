6275e619e01b7de3cb83146be1a75db622a60e7b
JeffreyAR <jar8@email.sc.edu>
2020-04-09 12:43:40 -0400

    Added equality message, improved comparison message.

--- Chapter4/drill.cpp
+++ Chapter4/drill.cpp
@@ -9,7 +9,8 @@ int main(){
   //integers.
   int i1,i2;
   while(cin >> i1 >> i2){
-    cout << "The smaller value is " << (i1 < i2 ? i1 : i2)
-         << "and the larger value is " << (i1 > i2 ? i1 : i2);
+    if(i1 == i2) cout << "The values are equal.\n";
+    else cout << "The smaller value is " << (i1 < i2 ? i1 : i2)
+         << " and the larger value is " << (i1 > i2 ? i1 : i2) << ".\n";
   }
 }
