7fc7dcedfa0b92cf0aa25b33cb18e4e7f38a05dc
JeffreyAR <jar8@email.sc.edu>
2020-04-09 13:11:35 -0400

    Now accepts four units - cm,m,in,ft. Converts to cm and compares.

--- Chapter4/drill.cpp
+++ Chapter4/drill.cpp
@@ -5,16 +5,28 @@ Author: Jeffrey Russell
 #include "../std_lib_facilities.h"
 
 int main(){
-  //Accept a double from the user. Continue running while the user supplies
-  //doubles.
+
+  //Initialize variables and conversion constants.
   double d1;
+  string unit;
   double small = 1;
   double large = 0;
-  while(cin >> d1){
+  const double METERS_TO_CENTIMETERS = 100;
+  const double INCHES_TO_CENTIMETERS = 2.54;
+  const double FEET_TO_CENTIMETERS = 12 * INCHES_TO_CENTIMETERS;
 
-    //Print out the double input from the user. Update the smallest and largest
-    //values if necessary while notifying the user of this change.
-    cout << d1 << "\n";
+  //Accept a double from the user. Continue running while the user supplies
+  //doubles.
+  while(cin >> d1 >> unit){
+
+    //Convert input to centimeters if necessary.
+    if(unit == "m") d1 *= METERS_TO_CENTIMETERS;
+    else if(unit == "in") d1 *= INCHES_TO_CENTIMETERS;
+    else if(unit == "ft") d1 *= FEET_TO_CENTIMETERS;
+
+    //Print out the double and unit input from the user. Update the smallest and
+    //largest values if necessary while notifying the user of this change.
+    cout << d1 << " cm" << "\n";
     if(small > large){
       small = d1;
       large = d1;
