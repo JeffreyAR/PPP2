f114cc9a4b2f05577364d7044f0dfde970940956
JeffreyAR <jar8@email.sc.edu>
2020-04-09 03:03:11 -0400

    Added comments to the Chapter 4 exercises, which I forgot.

--- Chapter4/AlphabetPrint.cpp
+++ Chapter4/AlphabetPrint.cpp
@@ -1,12 +1,16 @@
+/*
+Author: Jeffrey Russell
+Purpose: This program checks if an integer corresponds to an alphanumeric
+         character. If so, it prints the character and the integer. All
+         alphanumeric characters are printed.
+*/
+
 #include "../std_lib_facilities.h"
 
 int main(){
-  /*char iterate = 'a';
-  cout << iterate << "\t" << int(iterate) << "\n";
-  while(iterate != 'z'){
-    iterate = char(iterate + 1);
-    cout << iterate << "\t" << int(iterate) << "\n";
-  }*/
+
+  //Loop through the integers which contain alphanumeric characters. If the
+  //integer corresponds to an alphanumeric character, print both. 
   for(int i = 48; i <= 122; i++){
     if(isalnum(char(i))) cout << char(i) << " " << i << "\n";
   }
