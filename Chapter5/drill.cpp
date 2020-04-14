7b1ad600e42b956ebe8ab4f6795939a56b21c3ab
JeffreyAR <jar8@email.sc.edu>
2020-04-14 19:20:00 -0400

    Statement 9

--- Chapter5/drill.cpp
+++ Chapter5/drill.cpp
@@ -9,9 +9,9 @@ Purpose: This drill gives multiple ways of printing out "success" which will
 
 int main()
 try {
-  bool c = false;
-  if(!c) cout << "Success!\n";
-  else cout << "Fail!\n";
+  string s = "ape";
+  boo c = "fool" < s;
+  if(c) cout << "Success!\n";
   keep_window_open();
   return 0;
 }
