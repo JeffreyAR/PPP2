be8a101123eddc6b714a6c65ba39f0e02c405c35
JeffreyAR <jar8@email.sc.edu>
2020-04-14 19:30:30 -0400

    Removed "then".

--- Chapter5/drill.cpp
+++ Chapter5/drill.cpp
@@ -9,7 +9,7 @@ Purpose: This drill gives multiple ways of printing out "success" which will
 
 int main()
 try {
-  if(true) then cout << "Success!\n";
+  if(true) cout << "Success!\n";
   else cout << "Fail!\n";
   keep_window_open();
   return 0;
