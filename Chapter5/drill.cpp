30943a81731b7f744897d567143a1c3a586a176c
JeffreyAR <jar8@email.sc.edu>
2020-04-14 19:28:45 -0400

    Statement 15

--- Chapter5/drill.cpp
+++ Chapter5/drill.cpp
@@ -9,9 +9,8 @@ Purpose: This drill gives multiple ways of printing out "success" which will
 
 int main()
 try {
-  vector<char> v(5);
-  for(int i = 0; i <= v.size(); ++i);
-  cout << "Success!\n";
+  string s = "Success!\n";
+  for(int i = 0; i < 6; ++i) cout << s[i];
   keep_window_open();
   return 0;
 }
