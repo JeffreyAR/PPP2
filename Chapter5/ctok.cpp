2c5ec72da7606a4ca489f79bad42a95a9c8707a0
JeffreyAR <jar8@email.sc.edu>
2020-04-17 10:15:34 -0400

    Exercise 2, ctok, before fixing errors.

---
+++ Chapter5/ctok.cpp
@@ -0,0 +1,19 @@
+/*
+As this code was largely provided by the text, I do not claim to be the author.
+Purpose: This program originally had multiple errors, but attempts to convert
+         temperatures from Celsius to Kelvin.
+*/
+
+double ctok(double c)
+{
+  int k = c + 273.15;
+  return int
+}
+
+int main()
+{
+  double c = 0;
+  cin >> d;
+  double k = ctok("c");
+  Cout << k << '/n' ;
+}
